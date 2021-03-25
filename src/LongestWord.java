45e86658fa19bedf9f332b6ba2c0267c3ab75663
gilroyf <fernandes.gilroy@gmail.com>
2021-03-24 20:42:47 -0400

    initial commit

---
+++ src/LongestWord.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LongestWord {
+    public static String getLongestWord(String input, Set<String> dictionary) {
+        Map<Character, Integer> inputFrequencyMap = createFrequencyCountMap(input);
+        String longestWord = "";
+        for (String dictEntry : dictionary) {
+            if (canFormWord(dictEntry, inputFrequencyMap)) {
+                if (dictEntry.length() > longestWord.length())
+                    longestWord = dictEntry;
+            }
+        }
+        return longestWord;
+    }
+
+    private static Map<Character, Integer> createFrequencyCountMap(String input) {
+        return input.chars().boxed().collect(Collectors.toMap(
+                k -> Character.valueOf((char) k.intValue()),
+                v -> 1, Integer::sum
+        ));
+    }
+
+    private static boolean canFormWord(String word, Map<Character, Integer> inputFrequencyMap) {
+        Map<Character, Integer> wordFreqMap = createFrequencyCountMap(word);
+        for (Map.Entry<Character, Integer> wordEntry: wordFreqMap.entrySet())
+        {
+            if (!inputFrequencyMap.containsKey(wordEntry.getKey()))
+                return false;
+            if (wordEntry.getValue()  > inputFrequencyMap.get(wordEntry.getKey()))
+                return false;
+        }
+        return true;
+    }
+}
